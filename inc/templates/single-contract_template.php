6d0dbf3ba16ae41ba4f4242a273aea8a6d0dd36d
J7 <j7.dev.gg@gmail.com>
2024-12-04 11:50:37 +0800

    fix: 合約模板支援手機板

--- inc/templates/single-contract_template.php
+++ inc/templates/single-contract_template.php
@@ -18,7 +18,7 @@ $ajax_signed_btn_link = $settings_dto->ajax_signed_btn_link ?? '';
 
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
-		<meta name="viewport" content="<?php echo esc_attr( $viewport_content ); ?>">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="profile" href="https://gmpg.org/xfn/11">
 		<?php wp_head(); ?>
 	</head>
