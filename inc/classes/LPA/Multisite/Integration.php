27fb1da0dec4375527daa899fca0e1a73cdea2d6
J7 <j7.dev.gg@gmail.com>
2024-12-13 14:09:53 +0800

    feat: 可以替換子站合約的訂單資料

--- inc/classes/LPA/Multisite/Integration.php
+++ inc/classes/LPA/Multisite/Integration.php
@@ -11,6 +11,8 @@ namespace J7\PowerContract\LPA\Multisite;
 
 use J7\PowerContract\Resources\Contract\Init as Contract;
 use J7\PowerContract\Resources\ContractTemplate\Init as ContractTemplate;
+use J7\PowerContract\Admin\SettingsDTO;
+use J7\PowerContract\Utils\Base;
 
 if (class_exists('J7\PowerContract\LPA\Multisite\Integration')) {
 	return;
@@ -47,6 +49,9 @@ final class Integration {
 			\add_filter("manage_{$post_type}_posts_columns", [ $this, 'add_blog_column' ]);
 			\add_action("manage_{$post_type}_posts_custom_column", [ $this, 'display_blog_info' ], 10, 2);
 		}
+
+		// 要能帶子站的訂單參數
+		\add_filter('power_contract_input_args', [ __CLASS__, 'set_default_value' ], 100, 1);
 	}
 
 
@@ -87,5 +92,57 @@ final class Integration {
 				echo \esc_html($blog_details->blogname);
 			}
 		}
+	}
+
+
+	/**
+	 * 設定預設值
+	 *
+	 * @param array $args 輸入框的參數
+	 * @return array 輸入框的參數
+	 */
+	public static function set_default_value( array $args ): array {
+		$settings_dto = SettingsDTO::get_instance();
+		if (!$settings_dto->display_order_info) {
+			return $args;
+		}
+
+		$order_id = (int) $_GET['order_id'] ?? null; // phpcs:ignore
+		$blog_id  = (int) $_GET['blog_id'] ?? null; // phpcs:ignore 子站 id
+		if (!$order_id || !$blog_id) {
+			return $args;
+		}
+
+		// 切換到子站獲取資料
+		\switch_to_blog($blog_id);
+		$order = \wc_get_order($order_id);
+		$user  = $order->get_user();
+		if ( !$order || !$user) {
+			\restore_current_blog();
+			return $args;
+		}
+
+		// TODO 如果當前用戶與訂單用戶不同，就不自動填入，防止被猜出其他用戶個資
+		// $current_user_id = \get_current_user_id();
+		// if ($current_user_id !== $user->ID) {
+		// return $args;
+		// }
+
+		$first_name = $order->get_billing_first_name();
+		$last_name  = $order->get_billing_last_name();
+		$full_name  = "{$last_name}{$first_name}";
+
+		$value = match ($args['name']) {
+			'user_name' => $full_name,
+			'user_address' => Base::get_full_address($user->ID, 'shipping'),
+			'user_phone' => \get_user_meta($user->ID, 'billing_phone', true),
+			'contract_amount' => $order->get_total(),
+			default => '',
+		};
+
+		$args['value'] = $value;
+
+		\restore_current_blog();
+		return $args;
 	}
 }
