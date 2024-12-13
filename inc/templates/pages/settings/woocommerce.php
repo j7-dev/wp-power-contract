a00c96501c5949daac1acdc1cb3b7d1b9eba3a4f
J7 <j7.dev.gg@gmail.com>
2024-12-13 16:36:07 +0800

    feat: 新增設定

--- inc/templates/pages/settings/woocommerce.php
+++ inc/templates/pages/settings/woocommerce.php
@@ -5,6 +5,7 @@
 
 use J7\PowerContract\Admin\Settings;
 use J7\PowerContract\Admin\SettingsDTO;
+use J7\PowerContract\Resources\ContractTemplate\Init;
 
 
 $settings_key = Settings::SETTINGS_KEY;
@@ -15,17 +16,59 @@ printf(
 /*html*/'
 <sl-checkbox %1$s name="%2$s">%3$s</sl-checkbox>
 <br /><br />
-<sl-checkbox %4$s name="%5$s" disabled>%6$s</sl-checkbox>
+<sl-divider style="--width: 4px;"></sl-divider>
+<sl-checkbox %4$s name="%5$s">%6$s</sl-checkbox>
 <br /><br />
-<sl-checkbox %7$s name="%8$s" disabled>%9$s</sl-checkbox>
+<sl-checkbox %7$s name="%8$s">%9$s</sl-checkbox>
+<br /><br />
 ',
 $settings_dto->display_order_info ? 'checked' : '',
 SettingsDTO::get_field_name('display_order_info'),
 __('Display order information automatically', 'power_contract'),
 $settings_dto->display_contract_before_checkout ? 'checked' : '',
 SettingsDTO::get_field_name('display_contract_before_checkout'),
-__('Display Contract before checkout 開發中...', 'power_contract'),
+__('Display Contract before checkout', 'power_contract'),
 $settings_dto->display_contract_after_checkout ? 'checked' : '',
 SettingsDTO::get_field_name('display_contract_after_checkout'),
-__('Display Contract after checkout, before thank you page 開發中...', 'power_contract'),
+__('Display Contract after checkout, before thank you page', 'power_contract'),
+);
+
+
+$all_contracts = \get_posts(
+	[
+		'post_type'      => Init::POST_TYPE,
+		'post_status'    => 'publish',
+		'posts_per_page' => -1,
+	]
+	);
+
+
+if (!$all_contracts) {
+	return;
+}
+
+$options = '';
+
+foreach ($all_contracts as $contract) {
+	$options .= sprintf(
+	/*html*/'
+	<sl-option value="%1$s">%2$s</sl-option>
+	',
+	$contract->ID,
+	$contract->post_title,
+	);
+}
+
+
+printf(
+/*html*/'
+<sl-select label="%1$s" name="%2$s" help-text="%3$s" value="%5$s">
+	%4$s
+</sl-select>
+',
+__('Choose contract template', 'power_contract'),
+SettingsDTO::get_field_name('chosen_contract_template'),
+__('You can override the value by add filter `power_contract_chosen_contract_template` ', 'power_contract'),
+$options,
+$settings_dto->chosen_contract_template ?? '',
 );
