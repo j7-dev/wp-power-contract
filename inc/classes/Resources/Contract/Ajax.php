d8aff8236d06b7957264593f128c60f59a6b883f
J7 <j7.dev.gg@gmail.com>
2025-03-07 15:01:28 +0800

    feat: create_contract 紀錄 exception

--- inc/classes/Resources/Contract/Ajax.php
+++ inc/classes/Resources/Contract/Ajax.php
@@ -32,46 +32,49 @@ final class Ajax {
 	 * Create Contract
 	 */
 	public static function create_contract() {
-		// 驗證 nonce
-		if ( !\check_ajax_referer( Plugin::$kebab, 'nonce' ) ) {
-			\wp_send_json_error(
+
+		try {
+
+			// 驗證 nonce
+			if ( !\check_ajax_referer( Plugin::$kebab, 'nonce' ) ) {
+				\wp_send_json_error(
 				[
 					'title'       => 'OOPS! 合約簽屬中發生錯誤!',
 					'description' => 'nonce 錯誤或過期，請重新整理頁面',
 				]
-				);
-		}
+					);
+			}
 
-		// sanitize $_POST
-		unset( $_POST['nonce'] );
-		unset( $_POST['action'] );
-		$post_data               = WP::sanitize_text_field_deep( $_POST, false, [ 'screenshot', 'signature' ] );
-		$post_data['client_ip']  = General::get_client_ip();
-		$include_required_params = WP::include_required_params( $post_data, [ 'contract_template_id' ] );
-		if ( true !== $include_required_params ) {
-			\wp_send_json_error(
+			// sanitize $_POST
+			unset( $_POST['nonce'] );
+			unset( $_POST['action'] );
+			$post_data               = WP::sanitize_text_field_deep( $_POST, false, [ 'screenshot', 'signature' ] );
+			$post_data['client_ip']  = General::get_client_ip();
+			$include_required_params = WP::include_required_params( $post_data, [ 'contract_template_id' ] );
+			if ( true !== $include_required_params ) {
+				\wp_send_json_error(
 				[
 					'code'    => 'sign_error',
 					'message' => $include_required_params->get_error_message(),
 				]
-			);
-		}
+				);
+			}
 
-		$user_id   = self::get_user_id_by_bot_raw_id();
-		$user_name = $post_data['user_name'] ?? '未填寫姓名';
+			$user_id   = self::get_user_id_by_bot_raw_id();
+			$user_name = $post_data['user_name'] ?? '未填寫姓名';
 
-		$img_info = WP::upload_single_base64_image( $post_data['screenshot'], 'contract', true );
-		unset( $post_data['screenshot'] );
+			$img_info = WP::upload_single_base64_image( $post_data['screenshot'], 'contract', true );
+			unset( $post_data['screenshot'] );
 
-		[
+			[
 			'data' => $data,
 			'meta_data' => $meta_data
-		] = WP::separator($post_data, 'post');
+			] = WP::separator($post_data, 'post');
 
-		$contract_template_name      = \get_the_title( $meta_data['contract_template_id'] );
-		$meta_data['screenshot_url'] = $img_info['url'];
+			$contract_template_name      = \get_the_title( $meta_data['contract_template_id'] );
+			$meta_data['screenshot_url'] = $img_info['url'];
 
-		$data['meta_input'] = $meta_data;
+			$data['meta_input'] = $meta_data;
 		// phpcs:disable
 		$default_args       = [
 			'post_type'   => Init::POST_TYPE,
@@ -89,24 +92,36 @@ final class Ajax {
 		];
 		// phpcs:enable
 
-		$args = \wp_parse_args( $data, $default_args );
+			$args = \wp_parse_args( $data, $default_args );
 
-		// insert data
-		$new_contract_id = \wp_insert_post( $args );
+			// insert data
+			$new_contract_id = \wp_insert_post( $args );
 
 		\do_action( 'power_contract_contract_created', $new_contract_id, $args, $_POST['_blog_id'] ); // phpcs:ignore
 
-		$order_id     = ( (int) $args['meta_input']['_order_id'] ) ?? null;
-		$redirect     = $args['meta_input']['_redirect'] ?? null;
-		$redirect_url = self::get_redirect_url($order_id, $redirect);
+			$order_id     = ( (int) $args['meta_input']['_order_id'] ) ?? null;
+			$redirect     = $args['meta_input']['_redirect'] ?? null;
+			$redirect_url = self::get_redirect_url($order_id, $redirect);
 
-		\wp_send_json_success(
+			\wp_send_json_success(
 			[
 				'code'         => 'sign_success',
 				'message'      => '',
 				'redirect_url' => \apply_filters('power_contract_contract_created_redirect_url', $redirect_url, $new_contract_id, $order_id),
 			]
 			);
+		} catch (\Throwable $th) {
+
+			\J7\WpUtils\Classes\WC::log(
+				$th->getMessage(),
+				'create_contract exception',
+				'info',
+				[
+					'source' => 'power-contract',
+				]
+				);
+
+		}
 	}
 
 
